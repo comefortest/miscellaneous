b32d15a9afcd7546f7e01d0490103904052d17e1
comefortest <thethethethethethe@qq.com>
2013-02-06 14:53:51 +0800

    a enhanced "break" in scala

---
+++ xzy/comefortest/miscellaneous/Control.scala
@@ -0,0 +1,32 @@
+package xyz.comefortest.miscellaneous.control
+
+import scala.util.control.NoStackTrace
+
+private sealed case class MyBreakControl[T]( final val r: Option[T], final val obj: Breakable[T]) extends Exception;
+private sealed case class MyNothingBreakControl( final val obj: Breakable[_]) extends Exception //with NoStackTrace;
+private final class OverallNothingBreakControl extends Exception;
+
+class Breakable[T] {
+  def break(r: Option[T]) = { throw new MyBreakControl(r, this) }
+  def break = throw new MyNothingBreakControl(this);
+  def apply(f: => Option[T]): Option[T] = {
+    try {
+      f
+    } catch {
+      case MyBreakControl(r: Option[T], obj) if obj == this => r;
+      case MyNothingBreakControl(obj) if obj == this => None;
+      case _: OverallNothingBreakControl => None;
+    }
+  }
+}
+object Breakable {
+  def apply[T]() = { new Breakable[T] }
+  def break = { throw new OverallNothingBreakControl; }
+  def breakable(f: => Unit): Unit = {
+    try {
+      f
+    } catch {
+      case _: OverallNothingBreakControl => None;
+    }
+  }
+}
