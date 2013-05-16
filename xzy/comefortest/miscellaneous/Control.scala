508c22e71958cb3784c74d681f232ec0dd63dff3
comefortest <thethethethethethe@qq.com>
2013-05-16 17:27:37 +0800

    an enhanced "break" for scala
    1. you can specify which "control" to break;
    2. you can specify the result to return.

--- xzy/comefortest/miscellaneous/Control.scala
+++ xzy/comefortest/miscellaneous/Control.scala
@@ -1,34 +1,50 @@
 package xyz.comefortest.miscellaneous.control
 
-//an enhanced "break" in scala
+//an enhanced "break" for scala
 
 import scala.util.control.NoStackTrace
 
-private final case class MyBreakControl[T]( final val r: Option[T], final val obj: Breakable[T]) extends Exception;
-private final case class MyNothingBreakControl( final val obj: Breakable[_]) extends Exception //with NoStackTrace;
-private final class OverallNothingBreakControl extends Exception;
-
+private final case class MyBreakControl0[T](val r: Option[T], val obj: Breakable[T]) extends Exception //with NoStackTrace
 class Breakable[T] {
-  def break(r: Option[T]) = { throw new MyBreakControl(r, this) }
-  def break = throw new MyNothingBreakControl(this);
+  def break(r: Option[T]): Nothing = { throw new MyBreakControl0(r, this) }
+  def break: Nothing = break(None)
   def apply(f: => Option[T]): Option[T] = {
     try {
       f
     } catch {
-      case MyBreakControl(r: Option[T], obj) if obj == this => r;
-      case MyNothingBreakControl(obj) if obj == this => None;
-      case _: OverallNothingBreakControl => None;
+      case MyBreakControl0(r: Option[T], obj) if obj == this => r
+    }
+  }
+  def apply(f: => Unit): Unit = {
+    try {
+      f
+    } catch {
+      case MyBreakControl0(r: Option[T], obj) if obj == this => {}
     }
   }
 }
 object Breakable {
   def apply[T]() = { new Breakable[T] }
-  def break = { throw new OverallNothingBreakControl; }
+
+  private object DummyBreakControl extends Exception with NoStackTrace
+  def break: Nothing = { throw DummyBreakControl }
   def breakable(f: => Unit): Unit = {
     try {
       f
     } catch {
-      case _: OverallNothingBreakControl => None;
+      case x: MyBreakControl1[_] => {}
+      case DummyBreakControl => {}
+    }
+  }
+
+  private final case class MyBreakControl1[T](val r: Option[T]) extends Exception //with NoStackTrace
+  def break[T](r: Option[T]): Option[T] = { throw new MyBreakControl1(r) }
+  def breakable[T](f: => Option[T]): Option[T] = {
+    try {
+      f
+    } catch {
+      case MyBreakControl1(r: Option[T]) => r
+      case DummyBreakControl => None
     }
   }
 }
--- xzy/comefortest/miscellaneous/ControlTest.scala
+++ xzy/comefortest/miscellaneous/ControlTest.scala
@@ -1,14 +1,35 @@
 package xyz.comefortest.miscellaneous.control
 
 object ControlTest extends App {
-
+  import xyz.comefortest.miscellaneous.control._
   import Breakable._
-  val outter1 = Breakable[Int]()
-  val outter2 = Breakable[Int]()
-  val result = outter1 {
-    outter2 {
-      Some(2)
+
+  println("test0 ...")
+  val r0 = breakable {
+    val now = System.currentTimeMillis()
+    now % 5 match {
+      case 0 => Some(0)
+      case 1 => break(Option("abc"))
+      case 2 => break
+      case _ => Some(now)
     }
   }
-  for (value <- result) { println("result : " + value) }
+  println("r0: " + r0)
+
+  println("test1 ...")
+  val outter_x = Breakable[String]()
+  val outter_y = Breakable[Long]()
+  val r1 = outter_x {
+    outter_y {
+      val now = System.currentTimeMillis()
+      now % 5 match {
+        case 0 => Some(0l)
+        case 1 => outter_x.break(Option("abc"))
+        case 2 => break
+        case 3 => outter_y.break(Option(999))
+        case _ => Some(now)
+      }
+    }.map(_.+(1).toString)
+  }
+  println("r1: " + r1)
 }
